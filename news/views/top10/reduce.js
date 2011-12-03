45ad4c683da66592f350201ba210358208435611
Jim Northrup <northrup.james@gmail.com>
2011-12-02 18:28:08 -0800

    reduce

--- news/views/top10/reduce.js
+++ news/views/top10/reduce.js
@@ -2,10 +2,16 @@ function(keys, values) {
 
     var a = [];
     for (var i = 0; i < keys.length; i++) {
-        a.push({date: keys[i], id: values[i] });
+        a.push([ keys[i],  values[i] ]);
     }
     var sorta = a.sort(function(a, b) {
-        return a.date - b.date
+        return a[0]- b[0]
     });
-    return sorta.slice(0, 9);
+    var s = sorta.slice(0, 9);
+    var v = [];
+    for (var i = 0; i < s.length; i++) {
+        v .push(s[i][1]);
+    }
+
+    return v;
 }
--- src/main/java/com/enformable/client/NewsSite.java
+++ src/main/java/com/enformable/client/NewsSite.java
@@ -8,8 +8,6 @@ import com.google.gwt.user.client.ui.RootPanel;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class NewsSite implements EntryPoint {
-
-
     public void onModuleLoad() {
         RootPanel rootPanel = RootPanel.get();
         rootPanel.clear();
